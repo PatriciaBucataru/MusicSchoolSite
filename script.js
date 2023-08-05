35c0dc98a42cac6504b499eb492f193a7b2068fc
PatriciaBucataru <141512901+PatriciaBucataru@users.noreply.github.com>
2023-08-05 19:37:01 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const contactTd = document.getElementById("contact");
+const menuDiv = document.getElementById("menu");
+
+contactTd.addEventListener("click", () => {
+  menuDiv.style.backgroundColor = "blue";
+});
